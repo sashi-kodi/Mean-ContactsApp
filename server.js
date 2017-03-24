f1fee9d3f385bb454fe18ccc4079cf831c023d44
sashi-kodi <sashi.kodi@gmail.com>
2017-03-24 07:45:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ var app= express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
-//var data=[{name:'sashi', email:'sashikv@yahoo.com', phone:'4085060109'}, {name:'isha', phone:'4085060108',email:'sashikv@yahoo.com' }];
-//Connect to Mongoose
-mongoose.connect('mongodb://sashikodi:omsai28@ds151927.mlab.com:51927/whitebox');
+
+mongoose.connect('mongodb://sashikodi:omsai28@ds151927.mlab.com:51927/sandbox');
 var db=mongoose.connection;
 db.on('error', console.error.bind(console,'connection error'));
 db.once('open', function(){
